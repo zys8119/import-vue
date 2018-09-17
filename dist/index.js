161c740aa11e222b6327d2695f8102cb82403335
zys8119 <770959294@qq.com>
2018-09-17 16:30:56 +0800

    发布：v0.0.5

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+// const importVue = require("./import.js");
+import importVue from './import.js';
+export default ((config = {}, path, title, ex = {}) => {
+    const fileUrl = 'views/';
+    let configs = {
+        fileUrl: fileUrl,
+        name: "",
+        path: "/",
+        title: title
+    };
+    if (typeof config == "object") {
+        for (let i in config) {
+            configs[i] = config[i];
+        }
+    } else {
+        configs.name = config;
+        configs.path = path || `/${config.split("/").pop()}`;
+    }
+    for (let i in ex) {
+        configs[i] = ex[i];
+    }
+    return importVue(configs);
+});
